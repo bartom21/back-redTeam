5841a7c4262897d69514ecc7c682850b99b4d0c6
bartom21 <60236508+bartom21@users.noreply.github.com>
2022-10-04 23:40:17 -0300

    edit session

--- controllers/session.js
+++ controllers/session.js
@@ -4,6 +4,36 @@ const { db } = require("../firebase");
 
 const router = express.Router();
 
+exports.editSession= async (req, res, next) => {
+    console.log("editSession");
+    console.log(req.body)
+    try {
+        const { id } = req.params;
+        const data = req.body.appointment[id];
+        await db
+                .collection("sessions")
+                .doc(id)
+                .update(data);
+    /*const updated = await db
+                        .collection("sessions")
+                        .doc(id)
+                        .get().docs[0].data();*/
+    const sessionRef = db.collection('sessions').doc(id);
+    const doc = await sessionRef.get();
+    if (!doc.exists) {
+      console.log('No such document!');
+    } else {
+      console.log('Document data:', doc.data());
+      res.status(201).json({
+        id: id,
+        ...doc.data()
+    });
+    }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 exports.loadSessions= async (req, res, next) => {
         try {
           const querySnapshot = await db.collection("sessions").get();
@@ -20,18 +50,26 @@ exports.loadSessions= async (req, res, next) => {
 }
 
 exports.storeSession = async (req, res, next) => {
-    console.log(req.body)
+   //console.log(req.body)
     const appointment = req.body.appointment; 
     const title = appointment.title;
     const startDate = appointment.startDate;
     const endDate = appointment.endDate;
     const allDay = appointment.allDay;
+    const patient = appointment.patient;
+    const professional = appointment.professional;
+    const therapy = appointment.therapy;
+    const rRule = appointment.rRule ? appointment.rRule : null;
     try {
         await db.collection("sessions").add({
             title,
             startDate,
             endDate,
-            allDay
+            allDay,
+            patient,
+            professional,
+            therapy,
+            rRule
         });
         const querySnapshot = await db.collection("sessions").get();
         const appointments = querySnapshot.docs.map((doc) => ({
@@ -45,4 +83,3 @@ exports.storeSession = async (req, res, next) => {
         console.error(error);
       }
 }
-  
--- routes/sessionRoute.js
+++ routes/sessionRoute.js
@@ -5,9 +5,12 @@ const router = express.Router();
 const isAuth = require('../middleware/is-auth');
 const sessionController = require('../controllers/session');
 
+router.put('/session/:id', isAuth, sessionController.editSession);
+
 router.get('/calendar', sessionController.loadSessions);
 
 router.post('/session', isAuth, sessionController.storeSession);
+
 
 
 module.exports = router;
